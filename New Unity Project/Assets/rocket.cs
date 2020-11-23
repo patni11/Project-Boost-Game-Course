8dd0091850f7336f3fa3a551c4548e7d10576c25
shubh patni <shubhpatni2002@gmail.com>
2020-11-23 19:39:36 +0530

    Made objects resond
    Added Rocket, Music, Thrusters, and Directions using keys

---
+++ New Unity Project/Assets/rocket.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour
+{
+    AudioSource thrusters;
+    Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thrusters = GetComponent<AudioSource>();
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput(){
+        if (Input.GetKey(KeyCode.Space)){
+            rigidBody.AddRelativeForce(Vector3.up);
+            
+            if (!thrusters.isPlaying){
+            thrusters.Play();
+            }
+
+        }else{
+            thrusters.Stop();
+        }
+       
+        //thrusters.Stop();
+        if (Input.GetKey(KeyCode.A)){
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D)){
+            transform.Rotate(-Vector3.forward);
+        }
+
+    }
+}
